e61003e5fd4a1324b101903a06430fe3031f226b
sennem <marksenne000@gmail.com>
2018-10-24 15:08:05 -0400

    Create NavBar.js
    create the navbar

---
+++ src/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+/*import { Home } from "./Home";
+import { Mission } from "./Mission";
+import { Team } from "./Team";
+import { Add } from "./Add";*/
+
+export class NavBar extends React.Component {
+  /*goToAdd() {
+    ReactDOM.render(<Add />, document.getElementById("root"));
+  }
+  goToTeam() {
+    ReactDOM.render(<Team />, document.getElementById("root"));
+  }
+  goToMission() {
+    ReactDOM.render(<Mission />, document.getElementById("root"));
+  }
+  goToHome() {
+    ReactDOM.render(<Home />, document.getElementById("root"));
+  }*/
+  render() {
+    return (
+      <div className="NavBar-overall">
+        <div class="NavBar-blocks">Home</div>
+        <div class="NavBar-blocks">Mission</div>
+        <div class="NavBar-blocks">Team</div>
+        <div class="NavBar-blocks NavBar-add-an-idea">Add an Idea</div>
+      </div>
+    );
+  }
+}
