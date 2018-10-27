6b02149bde83d7a296d09f89aa5a1b89fd6d1f7c
Mark Senne <marksenne000@gmail.com>
2018-10-26 23:34:38 -0400

    Add team files
    add team files for team page

---
+++ src/Team.js
@@ -0,0 +1,385 @@
+import React from "react";
+import { NavBar } from "./NavBar";
+import "./Team.css";
+import mark from "./teampics/mark.jpg";
+import megan from "./teampics/megan.png";
+import will from "./teampics/will.JPG";
+import kip from "./teampics/kip.jpeg";
+import kevin from "./teampics/kevin.jpeg";
+import bumiller from "./teampics/bumiller.jpg";
+import dodd from "./teampics/dodd.jpg";
+import mccormick from "./teampics/mccormick.gif";
+import cooper from "./teampics/cooper.jpg";
+import xander from "./teampics/xander.jpeg";
+import jake from "./teampics/jake.jpeg";
+import quinn from "./teampics/quinn.jpeg";
+import savannah from "./teampics/savannah.jpeg";
+import sophia2 from "./teampics/sophia2.jpeg";
+import roberts from "./teampics/roberts.jpg";
+import sample from "./teampics/sample.jpg";
+import jordan from "./teampics/jordan.jpeg";
+import priyanka from "./teampics/priyanka.jpeg";
+import volpenhein from "./teampics/volpenhein.jpeg";
+import kirsten from "./teampics/kirsten.jpeg";
+import taylor from "./teampics/taylor.jpeg";
+import maddie from "./teampics/maddie.jpeg";
+import distel from "./teampics/distel.jpg";
+
+export class Team extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="Team-overall">
+          <div className="Team-student_section">
+            <span>Students</span>
+          </div>
+          <div className="Team-card">
+            <img src={megan} alt="megan" />
+            <span>Megan Cui</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Launch, Development</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={mark} alt="mark" />
+            <span>Mark Senne</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Development</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={will} alt="will" />
+            <span>Will Mechler</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Development</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={savannah} alt="savannah" />
+            <span>Savannah Quach</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Launch</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={kip} alt="kip" />
+            <span>Kip Roe</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Branding & PR</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={kevin} alt="kevin" />
+            <span>Kevin Tang</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Launch, Planning</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sophia2} alt="sophia" />
+            <span>Sophia Johnson</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Planning</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={xander} alt="xander" />
+            <span>Xander Wells</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Launch, Planning</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={jake} alt="jake" />
+            <span>Jake Snyder</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Branding & PR</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={quinn} alt="quinn" />
+            <span>Quinn Robinson</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Video Production</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={jordan} alt="jordan" />
+            <span>Jordan Loewe</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Planning</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={priyanka} alt="priyanka" />
+            <span>Priyanka Paradkar</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={kirsten} alt="kirsten" />
+            <span>Kirsten Riley</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Launch, Planning</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={maddie} alt="maddie" />
+            <span>Maddie Lin</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Planning</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={taylor} alt="taylor" />
+            <span>Taylor Kiss</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Launch</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={volpenhein} alt="sample" />
+            <span>Luke Volpenhein</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>Launch</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="sample" />
+            <span>sample</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>sample</i>
+            </span>
+          </div>
+
+          <div className="Team-admin_section">
+            <span>Administration</span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="bumiller" />
+            <span>Shanna Bumiller</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>administration</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={roberts} alt="roberts" />
+            <span>Aaron Roberts</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>administration</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={dodd} alt="dodd" />
+            <span>Bobby Dodd</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>administration</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={cooper} alt="cooper" />
+            <span>Jonathan Cooper</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>administration</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={sample} alt="mccormick" />
+            <span>Christine McCormick</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>administration</i>
+            </span>
+          </div>
+          <div className="Team-card">
+            <img src={distel} alt="distel" />
+            <span>Dan Distel</span>
+            <br />
+            <span>-</span>
+            <br />
+            <span>
+              <i>administration</i>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
