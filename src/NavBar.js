460503f66d631006da43c5148995cb2afb4199e2
Mark Senne <marksenne000@gmail.com>
2018-10-30 21:43:57 -0400

    add home to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./NavBar.css";
-/*import { Home } from "./Home";
-import { Mission } from "./Mission";*/
+import { Home } from "./home";
+/*import { Mission } from "./Mission";*/
 import { Team } from "./Team";
 import App from "./App";
 
@@ -15,14 +15,14 @@ export class NavBar extends React.Component {
   }
   /*goToMission() {
     ReactDOM.render(<Mission />, document.getElementById("root"));
-  }
+  }*/
   goToHome() {
     ReactDOM.render(<Home />, document.getElementById("root"));
-  }*/
+  }
   render() {
     return (
       <div className="NavBar-overall">
-        <div className="NavBar-blocks" onClick={this.goToAdd}>Home</div>
+        <div className="NavBar-blocks" onClick={this.goToHome}>Home</div>
         <div className="NavBar-blocks" onClick={this.goToTeam}>Team</div>
         <div className="NavBar-blocks NavBar-add-an-idea" onClick={this.goToAdd}>Add an Idea</div>
       </div>
